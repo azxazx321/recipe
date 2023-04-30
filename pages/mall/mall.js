58d2be36a6f08cc524cc89b1107bfebc1f9b3eca
luke <305867377@qq.com>
2023-04-30 18:54:56 +1000

    finish mall scroll function the previous version is pages

--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -1,7 +1,15 @@
 $(function () {
+
+    let currentPage = 1
+    let loading = false
     function getData(p){
+      if($('.nomore:visible').length == 1) return
+
+      if(loading) return
+      loading = true  
       var url = 'https://serverms.xin88.top/mall?page='+ p
     $.get(url, data => {
+      loading = false
       console.log('mall:', data);
       const content = data.data.map(
         element => {
@@ -17,11 +25,24 @@ $(function () {
           `
         }       
       )
-      $('.mall>.content').html(content)
+      //$('.mall>.content').html(content)
+      $('.mall>.content').append(content)
       
       $('.mall>.pages>ul').empty()
 
       const {page,pageCount} = data
+      currentPage = page
+
+      if(page == pageCount) {
+        $('.nomore').show()
+        $('.loading').hide()
+      } else {
+        $('.nomore').hide()
+        $('.loading').show()
+      }
+
+
+
       let start = page -2
       let end = page + 2
       
@@ -46,7 +67,7 @@ $(function () {
       p == pageCount ? $btn_next.hide() : $btn_next.show()
       p == 1 ? $btn_prev.hide() : $btn_prev.show()
 
-      $(window).scrollTop(0)
+      //$(window).scrollTop(0)
 
     })
 
@@ -74,4 +95,18 @@ $(function () {
   }
   )
    
+    $(window).scroll(function(){
+      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+            console.log('scroll')
+        }
+
+    })
+    $(window).scroll(function() {
+      if($(window).scrollTop() + $(window).height() +150 >= $(document).height()) {
+         console.log('bottom is already')
+         getData(currentPage + 1)
+      }
+   })
+   
+
   })
